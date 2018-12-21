0c60d9a60b780977e91f6f05d2f45d2eb70088e0
Thế Anh <theanhnb97@users.noreply.github.com>
2018-12-21 10:42:39 +0700

    Initial commit

---
+++ StartMenu/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace StartMenu
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            ShowStartMenu();
+            Application.Exit();
+        }
+        [DllImport("user32.dll")]
+        static extern void keybd_event(byte bVk, byte bScan, uint dwFlags,UIntPtr dwExtraInfo);
+        private const byte UP = 2;
+        private const byte CTRL = 17;
+        private const byte ESC = 27;
+        private static void ShowStartMenu()
+        {
+            // key down event:
+            const byte keyControl = 0x11;
+            const byte keyEscape = 0x1B;
+            keybd_event(keyControl, 0, 0,UIntPtr.Zero);
+            keybd_event(keyEscape, 0, 0, UIntPtr.Zero);
+
+            // key up event:
+            const uint KEYEVENTF_KEYUP = 0x02;
+            keybd_event(keyControl, 0, KEYEVENTF_KEYUP,UIntPtr.Zero);
+            keybd_event(keyEscape, 0, KEYEVENTF_KEYUP,UIntPtr.Zero);
+        }
+    }
+}
